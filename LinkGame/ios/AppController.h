21972235754af0e7af59e2080fb82799a7d85e5a
CpyShine <backjy@foxmail.com>
2013-04-19 16:37:01 +0800

    Initial Commit

---
+++ LinkGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  LinkGameAppController.h
+//  LinkGame
+//
+//  Created by CpyShine on 13-4-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ LinkGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LinkGameAppController.h
+//  LinkGame
+//
+//  Created by CpyShine on 13-4-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
